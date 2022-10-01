3e37e460131f1dcb99b0bb1a4862260bd9d71eb2
alexxandrrar <alexandroovnaa@gmail.com>
2022-10-02 00:40:35 +0300

    feat/ routers added

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
-import { Switch, Route } from "react-router-dom";
-
+import { Col, Row } from "antd";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-
+import { SideBar } from "./components/SideBar/SideBar";
 import { PaginationPage } from "./pages/PaginationPage/PaginationPage";
 
 import "antd/dist/antd.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route path="/pagination">
-          <PaginationPage />
-        </Route>
-      </Switch>
-      <Footer />
-    </div>
+    <Row>
+      <Col span={24}>
+        <Header />
+      </Col>
+      <Col span={3}>
+        <SideBar />
+      </Col>
+      <Col span={21}>
+        <Routes>
+          <Route path="/pagination" element={<PaginationPage />} />
+        </Routes>
+      </Col>
+      <Col span={24}>
+        <Footer />
+      </Col>
+    </Row>
   );
 }
 
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,19 +1,58 @@
-import { FC } from "react";
+import React, { useState } from "react";
+import { Menu, Divider, Button, Row, Col } from "antd";
 import { Link } from "react-router-dom";
+import {
+  SmileOutlined,
+  SettingOutlined,
+  MenuUnfoldOutlined,
+  AlignLeftOutlined,
+  ArrowLeftOutlined,
+} from "@ant-design/icons";
 
-const SideBar: FC = () => {
+export const SideBar: React.FC = () => {
+  const [isToggleCollapse, setIsToggleCollapse] = useState<boolean>(true);
+  const onCollapseHandler = () =>
+    setIsToggleCollapse((prevState: boolean) => !prevState);
+  const onBackHandler = () => window.history.back();
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/firstTask">Task 1</Link>
-        </li>
-        <li>
-          <Link to="/pagination">Task 2</Link>
-        </li>
-      </ul>
-    </nav>
+    <Menu
+      style={{ height: "100vh" }}
+      defaultSelectedKeys={["1"]}
+      mode={"vertical"}
+      inlineCollapsed={isToggleCollapse}
+    >
+      <Row>
+        <Col>
+          <Divider key={"7"} orientation={"left"}>
+            {!isToggleCollapse && (
+              <Button onClick={onBackHandler}>
+                <ArrowLeftOutlined />
+              </Button>
+            )}
+          </Divider>
+        </Col>
+        <Col>
+          <Divider key="5" orientation={"right"}>
+            <Button onClick={onCollapseHandler}>
+              {isToggleCollapse ? (
+                <MenuUnfoldOutlined />
+              ) : (
+                <AlignLeftOutlined />
+              )}
+            </Button>
+          </Divider>
+        </Col>
+      </Row>
+      <Menu.Item key="1" icon={<SmileOutlined />}>
+        <Link to={"/"}>Task 1</Link>
+      </Menu.Item>
+      <Menu.Item key="2" icon={<SmileOutlined />}>
+        <Link to={"/pagination"}>Task 2</Link>
+      </Menu.Item>
+      <Menu.Item key="3" icon={<SettingOutlined />}>
+        <Link to={"/settings"}>Settings</Link>
+      </Menu.Item>
+    </Menu>
   );
 };
-
-export default SideBar;
